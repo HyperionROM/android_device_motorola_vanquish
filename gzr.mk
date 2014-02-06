2995befc716392c9f6f6be5dbee7e1fe14ac5dca
Somber73 <nytesblood@gmail.com>
2014-02-06 16:04:34 -0500

    Update and rename cm.mk to gzr.mk

--- gzr.mk
+++ gzr.mk
@@ -1,8 +1,8 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/gzr/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/gzr/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -10,6 +10,6 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := MOROTOROLA RAZR HD
-PRODUCT_NAME := cm_vanquish
+PRODUCT_NAME := gzr_vanquish
 
 $(call inherit-product, device/motorola/vanquish/full_vanquish.mk)
